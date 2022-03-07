230c18a746b256c7b6ded379cf2cddfcab17946a
Yerba lover ;) <95034549+roadToVictory@users.noreply.github.com>
2022-03-08 00:14:44 +0100

    Update TableFrame.java

--- src/TableFrame.java
+++ src/TableFrame.java
@@ -3,9 +3,9 @@ import javax.swing.*;
 public class TableFrame extends JFrame {
  
  	public TableFrame(String table) {
- 		super("Dane " + table);
+ 		     super("Dane " + table);
         add(new Table(table));
- 		pack();
- 		setVisible(true);
+ 	     	pack();
+ 		     setVisible(true);
      }
  }
